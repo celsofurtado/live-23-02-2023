d6b05314ab25cd90179eb29419173f2958c3f164
Celso Furtado <celsofurtado@terra.com.br>
2023-02-23 16:46:26 -0300

    Somente UI

---
+++ app/src/main/java/br/senai/sp/jandira/fiap/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.senai.sp.jandira.fiap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.senai.sp.jandira.fiap.R.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
